cbf587464b483a2492adbe320202aea7889bc5c2
Max Swietnicki <swietnickimax@gmail.com>
2025-12-21 12:55:28 -0500

    Binder reward change

--- src/main/java/spireQuests/quests/dayvig/relics/Binder.java
+++ src/main/java/spireQuests/quests/dayvig/relics/Binder.java
@@ -1,8 +1,5 @@
 package spireQuests.quests.dayvig.relics;
 
-import com.megacrit.cardcrawl.actions.animations.VFXAction;
-import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
-import com.megacrit.cardcrawl.vfx.RainingGoldEffect;
 import spireQuests.abstracts.AbstractSQRelic;
 
 import static spireQuests.Anniv8Mod.makeID;
@@ -17,14 +14,5 @@ public class Binder extends AbstractSQRelic {
         super(ID, "dayvig", RelicTier.SPECIAL, LandingSound.FLAT);
     }
 
-
-    @Override
-    public void onEquip() {
-        int targetMaxHP = (int) Math.ceil((double)AbstractDungeon.player.maxHealth / DIVISOR) * DIVISOR;
-        int targetGold = (int)Math.ceil((double)AbstractDungeon.player.gold / DIVISOR) * DIVISOR;
-        AbstractDungeon.player.increaseMaxHp(targetMaxHP - AbstractDungeon.player.maxHealth, true);
-        AbstractDungeon.player.gainGold(targetGold - AbstractDungeon.player.gold);
-        addToBot(new VFXAction(new RainingGoldEffect(targetGold - AbstractDungeon.player.gold)));
-    }
 }
 
